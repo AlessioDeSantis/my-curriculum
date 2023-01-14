af86017299ea874111102cfa30f090ca83b9d49c
Alessio De Santis <alex.desantis2000@gmail.com>
2023-01-14 15:21:41 +0100

    sistemata sezione work

--- resources/views/components/work.blade.php
+++ resources/views/components/work.blade.php
@@ -11,8 +11,8 @@
 
     <div class="container">
         <div class="row justify-content-center">
-            <div class="col-lg-4 col-md-6 col-sm-12 my-5 text-center">
-                <h3 class="title-2-container py-3 px-2 rounded shadow">Work experience</h3>
+            <div class="col-lg-4 col-md-6 col-sm-6 my-5 text-center">
+                <h3 class="title-2-container py-3 px-2 shadow">Work experience</h3>
             </div>
 
         </div>
@@ -23,28 +23,49 @@
                     <div class="carousel-indicators">
                         <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
                         <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                        <!-- <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button> -->
                     </div>
                     <div class="carousel-inner">
                         <div class="carousel-item active">
-                        <div class="carousel-caption rounded shadow">
-                            <h5>First slide label</h5>
+                        <div class="carousel-caption shadow p-2">
+                            <h5>Addetto alla vigilanza non armata</h5>
                             <hr>
-                            <p>Some representative placeholder content for the first slide.</p>
+                            <p class="text-start">
+                                <i class="fa-solid fa-calendar-days"></i>  December-2020 at April-2022 <br>
+                                <i class="fa-solid fa-briefcase"></i>  Innovative Srl <br>
+                                <i class="fa-solid fa-location-dot"></i> Fiano Romano, Roma <br>
+                                <div class="mt-2 text-start fs-1">
+                                    <h5>Un lavoro dove è necessario:</h5>
+                                    
+                                    <ul>
+                                        <li>Il monitoraggio degli ingressi e delle uscite del personale e dei lavoratori esterni;</li>
+                                        <li>Garantire il regolare svolgimento del lavoro all'interno dell'edificio risolvendo problemi tecnici;</li>
+                                        <li>Avere cura del proprio ambiente di lavoro;</li>
+                                    </ul>
+                                </div>
+                            </p>
                         </div>
                         </div>
                         <div class="carousel-item">
-                        <div class="carousel-caption rounded shadow">
-                            <h5>Second slide label</h5>
+                        <div class="carousel-caption shadow p-2">
+                            <h5>Aiuto allenatore di calcio</h5>
                             <hr>
-                            <p>Some representative placeholder content for the second slide.</p>
-                        </div>
-                        </div>
-                        <div class="carousel-item">
-                        <div class="carousel-caption rounded shadow">
-                            <h5>Third slide label</h5>
-                            <hr>
-                            <p>Some representative placeholder content for the third slide.</p>
+                            <p class="text-start">
+                                <i class="fa-solid fa-calendar-days"></i>  September-2019 at now <br>
+                                <i class="fa-solid fa-briefcase"></i>  Vigor Rignano Flaminio <br>
+                                <i class="fa-solid fa-location-dot"></i> Rignano Flaminio, Roma <br>
+                                <div class="mt-2 text-start fs-1">
+                                    <h5>Un'attività che insegna molto dal punto di vista professionale, come:</h5>
+                                    
+                                    <ul>
+                                        <li>Gestire un gruppo e lavorare per un obbiettivo comune;</li>
+                                        <li>Prendersi le responsabilità dovute all'organizzazione di un evento;</li>
+                                        <li>Saper gestire le difficoltà, anche sotto pressione;</li>
+                                        <li>Sapersi mettere in discussione imparando dai propri errori;</li>
+                                        <li>Autovalutazione del proprio operato, verificabile durante le competizioni;</li>
+                                    </ul>
+                                </div>
+                            </p>
                         </div>
                         </div>
                     </div>
